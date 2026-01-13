0c1e2eeb75f9409de244a8a74ef2abbea4b5d6e4
nthuong100103-cell <nthuong100103@gmail.com>
2026-01-13 17:13:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import joblib
+import os
 
 # =====================
 # Load artifacts
@@ -114,4 +115,5 @@ if st.button("🔮 Dự đoán"):
             "Xác suất": probability
         })
     )
+
 
